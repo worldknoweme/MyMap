4e0a964ada5d1faaa37a32a93294a39664bfee45
wang-jl <wangjianlin1989@163.com>
2018-03-13 19:58:45 +0800

    1、修改手机不支持64位的bug 2、获取定位信息修改，如果不能正常定位，请手动修改app权限，使其获取定位权限

--- app/src/main/java/com/cx/mymap/MapActivity.java
+++ app/src/main/java/com/cx/mymap/MapActivity.java
@@ -2,14 +2,14 @@ package com.cx.mymap;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.MapStatus;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
@@ -20,7 +20,9 @@ public class MapActivity extends Activity {
     private MapView mMapView = null;
 
     private BaiduMap bdMap;
-    public LocationClient mLocationClient = null;
+    //定位相关
+    private LocationClient mLocationClient;
+    private MyLocationListener mLocationListener;
     boolean firstLoc=true; //是否首次定位
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,52 +34,65 @@ public class MapActivity extends Activity {
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
         bdMap = mMapView.getMap();
-
-        bdMap.setMyLocationEnabled(true);//开启定位图层
-        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
-        mLocationClient.registerLocationListener(mLocationClientListener);
-        mLocationClient.start();
+        initLocation();
     }
 
-    private BDLocationListener mLocationClientListener =new BDLocationListener() {
+    private void initLocation() {
+        //定位客户端的设置
+        mLocationClient = new LocationClient(this);
+        mLocationListener = new MyLocationListener();
+        //注册监听
+        mLocationClient.registerLocationListener(mLocationListener);
+        //配置定位
+        LocationClientOption option = new LocationClientOption();
+        option.setCoorType("bd09ll");//坐标类型
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//打开Gps
+        option.setScanSpan(1000);//1000毫秒定位一次
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        mLocationClient.setLocOption(option);
+    }
 
-
+    //自定义的定位监听
+    private class MyLocationListener implements BDLocationListener {
         @Override
-        public void onReceiveLocation(BDLocation arg0) {
-            // TODO Auto-generated method stub
-            if(arg0==null)
-                return;
-
-            float  accuracy= arg0.getRadius();
-            double  lat=  arg0.getLatitude();
-            double  lon= arg0.getLongitude();
-
-            MyLocationData.Builder build=new  MyLocationData.Builder();
-            build.accuracy(accuracy);
-            build.latitude(lat);
-            build.longitude(lon);
-            MyLocationData myLocationData=build.build();
-            bdMap.setMyLocationData(myLocationData);//设置定位数据
-
+        public void onReceiveLocation(BDLocation location) {
+            //将获取的location信息给百度map
+            MyLocationData data = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100)
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude())
+                    .build();
+            bdMap.setMyLocationData(data);
             if (firstLoc) {
+                //获取经纬度
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatusUpdate status = MapStatusUpdateFactory.newLatLng(ll);
+                //mBaiduMap.setMapStatus(status);//直接到中间
+                bdMap.animateMapStatus(status);//动画的方式到中间
                 firstLoc = false;
-
-                MapStatus.Builder mapStatusBuilder = new MapStatus.Builder();
-                LatLng ll = new LatLng(lat, lon);
-                mapStatusBuilder.target(ll);
-                //mapStatusBuilder.zoom(18.0f);
-
-                MapStatus mapStatus = mapStatusBuilder.build();
-                MapStatusUpdate mapUpdate = MapStatusUpdateFactory
-                        .newMapStatus(mapStatus);
-                bdMap.animateMapStatus(mapUpdate);
-
+                showInfo("位置：" + location.getAddrStr());
             }
         }
-    };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //开启定位
+        bdMap.setMyLocationEnabled(true);
+        if(!mLocationClient.isStarted()){//如果定位client没有开启，开启定位
+            mLocationClient.start();
+        }
+    }
 
 
-
+    //显示消息
+    private void showInfo(String str){
+        Toast.makeText(MapActivity.this,str, Toast.LENGTH_SHORT).show();
+    }
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
